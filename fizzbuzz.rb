e4ee4e7a81bd47582d49ac6650296eaf894a181c
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-10 20:02:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number % 3 == 0 && 
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  elsif number % 3 == 0
     return "Fizz"
-  elsif number % 5 == 0
-    return "Buzz"
-    elsif number %
+    elsif number % 5 == 0
+return "Buzz"
 else nil
 end
